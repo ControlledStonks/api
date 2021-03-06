8720e1e4af144f854278e84b6db32d88308baf72
JMcB <joel.mcbride1@live.com>
2021-03-06 16:01:41 +0000

    Create api

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+import bmemcached
+from flask import Flask, jsonify
+
+memcache = bmemcached.Client(os.environ['MEMCACHEDCLOUD_SERVERS'].split(','),
+                             os.environ['MEMCACHEDCLOUD_USERNAME'],
+                             os.environ['MEMCACHEDCLOUD_PASSWORD'])
+app = Flask(__name__)
+
+
+@app.route('/emote.json')
+def emote_json():
+    return jsonify(memcache.get('emote'))
